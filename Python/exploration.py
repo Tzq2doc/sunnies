f4fd81f1679ec5beb105f03e8bf238a559ccccd7
Inga <inga.struemke@pwc.com>
2020-02-23 17:41:09 +0100

    haha, got stuck. Stoopid Inga

--- Python/exploration.py
+++ Python/exploration.py
@@ -7,6 +7,7 @@ pip install dcor (from pypi.org/project/dcor/)
 
 """
 import numpy
+from itertools import combinations
 
 def make_y(x1, x2, x3, x4):
     a1, a2, a3, a4 = 0, 1, 3, 4
@@ -23,21 +24,37 @@ y = x*x
 
 #import simple_shapley from simple_shapley_helpers
 
-# For calculating the shapley value of player v
-value = 0
-for s in team_sizes: # These are all possible team sizes excluding v, i.e., (0,1,...,d-1)
-  value_s = 0
-  # <do> Create teams_of_size_s which stores all teams of size s (none of which include v)
-  for team in teams_of_size_s:
-    value_in_team = CF(y, X, append(v,team)) - CF(y, X, team) # <do> write CF
-    value_s += value_in_team
-  average_value_s = value_s/len(teams_of_size_s)
-  value += average_value_s
-average_value = value/len(team_sizes) # len(team_sizes) = d
+def calc_shap(players, v):
+    """
+    Calculate the Shapley value for v given a list of players in the game
+    """
+    assert isinstance(players, list)
+    if v in players:
+        players.remove(v)
 
+    num_players = len(players)
+    team_sizes = list(range(1, num_players+1)) # Python stops one before last
+    value = 0
+
+    for _size in team_sizes:
+        value_s = 0
+        teams_of_size_s = combinations(players, _size)
+        print(list(teams_of_size_s))
+        for _team in teams_of_size_s:
+            value_in_team = CF(y, X, append(v, _team)) - CF(y, X, _team) # <do> write CF
+            value_s += value_in_team
+        average_value_s = value_s/len(teams_of_size_s)
+        value += average_value_s
+    average_value = value/len(team_sizes) # len(team_sizes) = d
+
+players = [1,2,3,4]
+v =3
+print(calc_shap(players, v))
 
 
 # You can also precalculate this for efficiency.
 # Writing the characteristic function CF:
 def CF(y, X, team):
   # distance correlation between y and X
+  #oh no, what are y and X in this context :S
+  return
