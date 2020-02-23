76745b19dd6887b16e423c8d7e0c4d4647f48786
Inga <inga.struemke@pwc.com>
2020-02-23 20:24:34 +0100

    WE NEED TO TEST THIS, DANNY

--- Python/exploration.py
+++ Python/exploration.py
@@ -8,21 +8,27 @@ pip install dcor (from pypi.org/project/dcor/)
 """
 import numpy
 from itertools import combinations
+import dcor
+import sys
 
-def make_y(x1, x2, x3, x4):
-    a1, a2, a3, a4 = 0, 1, 3, 4
-    return a1*x1*x1 + a2*2*x2 + a3*x3*x3 + a4*x4*x4
+#def make_y(x1, x2, x3, x4):
+#    a1, a2, a3, a4 = 0, 1, 3, 4
+#    return a1*x1*x1 + a2*2*x2 + a3*x3*x3 + a4*x4*x4
 
-x = numpy.random.uniform(-1, 1, 100)
-y = x*x
-#cov(x,y) = 0
+n=10
+d=4
+X = numpy.array([[numpy.random.uniform(-1, 1) for x in range(d+1)]
+                  for y in range(n)])
+#y = X * (2*(0:(d-1))) + rnorm(n)
+e = e = numpy.random.uniform(-1, 1, n).reshape(n, 1) # Check this!
+y = X * 2*numpy.array(range(d+1)) + e # Square X at some point
 
-#n=1000, d=4
-# Matrix with (n rows, d columns) uniformly distributed (-1, 1)  reshape into nxd matrix
-#square element-wise
-#
-
-#import simple_shapley from simple_shapley_helpers
+# You can also precalculate this for efficiency.
+def CF(y, X, team):
+    """Distance correlation between y and X """
+    X = X[:, team]
+    y = y[:, team]
+    return dcor.distance_correlation(y,X)
 
 def calc_shap(players, v):
     """
@@ -38,23 +44,17 @@ def calc_shap(players, v):
 
     for _size in team_sizes:
         value_s = 0
-        teams_of_size_s = combinations(players, _size)
-        print(list(teams_of_size_s))
+        teams_of_size_s = list(combinations(players, _size)) #NB: returns tuples
         for _team in teams_of_size_s:
-            value_in_team = CF(y, X, append(v, _team)) - CF(y, X, _team) # <do> write CF
+            _team = list(_team) # cast tuple to list. probably a slow fix :(
+            value_in_team = CF(y, X, _team.append(v)) - CF(y, X, _team)
             value_s += value_in_team
         average_value_s = value_s/len(teams_of_size_s)
         value += average_value_s
     average_value = value/len(team_sizes) # len(team_sizes) = d
+    return average_value
 
 players = [1,2,3,4]
-v =3
+v = 3
 print(calc_shap(players, v))
 
-
-# You can also precalculate this for efficiency.
-# Writing the characteristic function CF:
-def CF(y, X, team):
-  # distance correlation between y and X
-  #oh no, what are y and X in this context :S
-  return
