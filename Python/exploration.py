3c3989a5a90287f923da11ae27682039b5c1aff1
Inga <inga.struemke@pwc.com>
2020-03-22 15:45:22 +0100

    Tidied up code

--- Python/exploration.py
+++ Python/exploration.py
@@ -11,15 +11,6 @@ from itertools import combinations
 import dcor
 import sys
 
-N = 10
-D = 5
-#X = numpy.array([[numpy.random.uniform(-1, 1) for _ in range(D)]
-#                  for y in range(N)])
-X = numpy.array([numpy.linspace(-1, 1, N) for _ in range(D)]).T
-#ERROR = numpy.random.normal(0, 1, N)
-Y = numpy.matmul(numpy.multiply(X, X), 2*numpy.array(range(D)))# + e
-V = 2
-
 def CF(x, y, team):
     """Distance correlation between y and X """
     x = x[:, team]
@@ -42,15 +33,11 @@ def make_cf_dict(x, y, players):
 
     return cf_dict
 
-def calc_shap(x, y, v, cf_dict, cf):
+def calc_shap(x, y, v, cf_dict):
     """
     Calculate the Shapley value for player indexed v,
     given X (todo explain) and the caracteristic function cf (todo explain)
     """
-    # If list of players given as input:
-    #todo assert only unique values via set(players)
-    #assert isinstance(players, list)
-
     players = list(range(X.shape[1]))
 
     if v in players:
@@ -63,17 +50,28 @@ def calc_shap(x, y, v, cf_dict, cf):
 
     for _size in team_sizes:
         value_s = 0
-        teams_of_size_s = list(combinations(players, _size)) #NB: returns tuples
+        teams_of_size_s = list(combinations(players, _size))
         for _team in teams_of_size_s:
-            value_in_team = cf(x, y, _team + v_tuple) - cf(x, y, _team)
-            #value_in_team = (cf_dict[tuple(sorted(_team+v_tuple))] - cf_dict[_team])
+            #value_in_team = cf(x, y, _team + v_tuple) - cf(x, y, _team)
+            value_in_team = (cf_dict[tuple(sorted(_team+v_tuple))] - cf_dict[_team])
             value_s += value_in_team
         average_value_s = value_s/len(teams_of_size_s)
         value += average_value_s
-    average_value = value/len(team_sizes) # len(team_sizes) = d
+    average_value = value/len(team_sizes)
     return average_value
 
-PLAYERS = list(range(D))
-CF_DICT = make_cf_dict(X, Y, PLAYERS)
-print(calc_shap(X, Y, V, CF_DICT, CF))
+if __name__ == "__main__":
+    N = 10
+    D = 5
+
+    #X = numpy.array([[numpy.random.uniform(-1, 1) for _ in range(D)]
+    #                  for y in range(N)])
+    X = numpy.array([numpy.linspace(-1, 1, N) for _ in range(D)]).T
+    #ERROR = numpy.random.normal(0, 1, N)
+    Y = numpy.matmul(numpy.multiply(X, X), 2*numpy.array(range(D)))# + e
+    V = 2
+
+    PLAYERS = list(range(D))
+    CF_DICT = make_cf_dict(X, Y, PLAYERS)
+    print(calc_shap(X, Y, V, CF_DICT))
 
