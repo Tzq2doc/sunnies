88aaaa9900ca377c51e457baceb2ebc1edce6217
Daniel Vidali Fryer <20966023+frycast@users.noreply.github.com>
2020-03-23 03:40:55 +1100

    Update utility_functions.R

--- R/utility_functions.R
+++ R/utility_functions.R
@@ -1,3 +1,6 @@
+# R squared
 R2 <- function(y, X) {if (length(X) == 0) {0} else {summary(speedlm(y~X))$r.squared}}
+# Distance correlation
 DC <- function(y, X){if (length(X) == 0) {0} else {dcor(y,X)$dcor}}
+# Bias corrected distance correlation
 BCDC <- function(y, X){if (length(X) == 0) {0} else {bcdcor(y,X)}}
