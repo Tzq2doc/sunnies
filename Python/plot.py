9e54b968a58d1e41166e587f2df37234a8bf69f2
Strumke <inga@strumke.com>
2020-04-16 07:25:56 +0200

    I dont know how but I think it works

---
+++ Python/plot.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import numpy
+import sys
+
+D = 4
+N = 100
+
+X = numpy.array([numpy.random.uniform(-1, 1, N) for _ in range(D)]).T
+#X = numpy.array([numpy.linspace(-1, 1, N) for _ in range(D)]).T
+TWO_D = 2*numpy.array(range(D))
+
+Y = numpy.matmul(numpy.multiply(X, X), TWO_D)
+
+#print(X)
+#print(numpy.multiply(X,X))
+#print(TWO_D)
+#print(Y)
+
+#print(numpy.array(range(D)).T)
+#print(X[:,1])
+
+plt.scatter(X[:,0], Y, label="X0", alpha=0.8)
+plt.scatter(X[:,1], Y, label="X1", alpha=0.8)
+plt.scatter(X[:,2], Y, label="X2", alpha=0.8)
+plt.scatter(X[:,3], Y, label="X3", alpha=0.8)
+plt.legend(loc="upper right")
+plt.show()
