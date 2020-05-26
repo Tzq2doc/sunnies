0464b3645282b7079833c4d36fcea63b5f9aff38
Strumke <inga@strumke.com>
2020-05-26 12:29:46 +0200

    new data. run quick_analysis to have a look

--- RL_data/data_analysis.py
+++ RL_data/data_analysis.py
@@ -1,7 +1,17 @@
 
-import pandas as pd
-import numpy
 import sys
+from typing import Union
+from itertools import combinations
+from matplotlib.colors import LinearSegmentedColormap, ListedColormap, Colormap
+from xgboost import XGBRegressor, XGBClassifier, plot_importance
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, mean_squared_error
+import numpy
+import matplotlib.pyplot as plt
+import shap
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+
 sys.path.insert(0, "../Python")
 from xgb_regressor import (display_predictions, display_feature_importances,
     display_shapley, display_shapley_vs_xgb, display_shap,
@@ -9,15 +19,39 @@ from xgb_regressor import (display_predictions, display_feature_importances,
 
 #data = pd.read_csv("../RL_data/student-mat.csv")
 data = pd.read_csv("student-mat.csv")
+target = "G3"
 
 # ----------------------------------------------------
-print(data.columns)
-print("Dropping most of the data here because it's type string.")
-data = data.select_dtypes([numpy.number])
-print(data.columns)
+#print(data.columns)
+
+
+# ----------------------------------------------------
+X_cat = data.select_dtypes(include=['object'])
+X_enc = X_cat.copy()
+
+# ----------------------------------------------------
+# --- One-hot encoding non-numerical data for XGB
+X_onehot = pd.get_dummies(X_cat, columns=X_cat.columns)
+#print(X_onehot.head)
 # ----------------------------------------------------
 
-X, y = data.iloc[:, :-1], data.iloc[:, -1]
+# ----------------------------------------------------
+# --- Label encoding data for Sunnies
+X_encoded = X_cat.apply(LabelEncoder().fit_transform) #
+#print(X_encoded.head)
+# --- End label encoding
+# ----------------------------------------------------
+data = data.drop(X_cat.columns, axis=1)
+sunnies_data = pd.concat([data, X_encoded], axis=1)
+xgb_data = pd.concat([data, X_onehot], axis=1)
+
+#print(sunnies_data.columns)
+#print(sunnies_data.head)
+#print(xgb_data.columns)
+
+
+y = sunnies_data[target].astype('int')
+X = sunnies_data.drop([target], axis=1)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=123)
 
@@ -41,11 +75,19 @@ residuals = y_test - y_pred
 #print("Saved file {0}.npy".format(filename))
 # ---
 
-#display_predictions(y_test, y_pred)
-display_feature_importances(model)
-display_shapley(X_train, y_train, cf=["dcor", "aidc"])#, "r2"])#"hsic"])
+print(X.columns)
+print(X.columns[14])#, 12, 13, 22, 4, 0, 6, 8, 27, 24])
+sys.exit()
+
+display_predictions(y_test, y_pred)
+print("1")
+display_feature_importances(model) # XGB built-in
+#display_shapley(X_train, y_train, cf=["dcor"])#, "aidc"])#, "r2"])#"hsic"])
+print("2")
 #display_shapley_vs_xgb()
-display_shap(X_test, model)
+print("3")
+#display_shap(X_test, model)
 #display_residuals_shapley(X_test, residuals)
+print("4")
 
 plt.show()
--- RL_data/quick_analysis.py
+++ RL_data/quick_analysis.py
@@ -4,16 +4,28 @@ import xgboost as xgb
 import numpy as np
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 
-data = pd.read_csv("student-mat.csv")
+#0, 3,4,7,9,14
+data = pd.read_csv('processed.cleveland.data', sep=",", header=0)
+#data = pd.read_csv("student-mat.csv")
 
 # ----------------------------------------------------
 print(data.columns)
-print("Dropping most of the data here because it's type string.")
-data = data.select_dtypes([np.number])
-print(data.columns)
+#print("Dropping most of the data here because it's type string.")
+#data = data.select_dtypes([np.number])
+#print(data.columns)
 # ----------------------------------------------------
+data_cont = data.iloc[:, [0,1,3,4,7,9,13]]
+
+print(data_cont)
+sns.pairplot(data_cont, hue='sex', size=2.5)
+plt.show()
+
+sys.exit()
+
 
 X, y = data.iloc[:, :-1], data.iloc[:, -1]
 data_dmatrix = xgb.DMatrix(data=X,label=y)
@@ -23,7 +35,6 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 print(type(X_train))
 X_train=np.array(X_train)
 print(type(X_train))
-sys.exit()
 
 xg_reg = xgb.XGBRegressor(colsample_bytree = 0.3, learning_rate = 0.1,
         max_depth = 5, alpha = 10, n_estimators = 10)
