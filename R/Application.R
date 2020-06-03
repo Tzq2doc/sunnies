b04d3a981b385eff700f5305cfcaed6be325c1ca
Daniel Vidali Fryer <20966023+frycast@users.noreply.github.com>
2020-06-03 21:40:53 +1000

    calc_n1m function

--- R/Application.R
+++ R/Application.R
@@ -10,6 +10,32 @@ library(naniar)
 library(ggplot2)
 
 
+# Python data and python model --------------------------------------------
+
+dat_python_tr <- read.csv("../RL_data/train_data_5050.csv") # even male/female
+dat_python_te <- read.csv("../RL_data/test_data_9010.csv") # 90% women and 10% men
+
+y_ptrain <- as_tibble(dat_python_tr[,ncol(dat_python_tr)])
+X_ptrain <- as_tibble(dat_python_tr[,-ncol(dat_python_tr)])
+y_ptest <- as_tibble(dat_python_te[,ncol(dat_python_te)])
+X_ptest <- as_tibble(dat_python_te[,-ncol(dat_python_te)])
+X_ptrain <- mutate_if(X_ptrain, is.factor, ~ as.integer(.x)[.x]-1L)
+X_ptest <- mutate_if(X_ptest, is.factor, ~ as.integer(.x)[.x]-1L)
+nrow(X_ptrain) # 7000
+nrow(y_ptrain) # 7000
+nrow(X_ptest)  # 4400
+nrow(y_ptest)  # 4400
+
+# Yeah nah this looks bad
+xgb_python <- xgb.load("../RL_data/full_model.dat")
+predict(xgb_python, as.matrix(X_ptrain))
+
+
+
+# RAW data ----------------------------------------------------------------
+
+
+
 Xh <- read.csv("../RL_data/X_data_with_header.csv")
 X <- read.csv("../RL_data/X_data.csv", header = F)
 y <- read.csv("../RL_data/y_data.csv", header = F)
@@ -21,6 +47,11 @@ names(Xh)
 nrow(Xh)
 nrow(X)
 nrow(y)
+sum(Xh[["sex_isFemale"]] == F)
+
+
+
+
 
 
 #### MISSINGNESS
@@ -64,7 +95,6 @@ barplot(s,
         xlab = "Feature", ylab = "Attribution",
         col = c("black","gray"), beside = T)
 
-
 # Comparing some features between all male and all female (in X_dc)
 interesting <- c(
   "sex_isFemale", "age", "physical_activity", "systolic_blood_pressure")
@@ -96,10 +126,43 @@ diagn <- diagnostics(sdat, xgbt, plot = "all",
 s3 <- shapley(sdat$y_train, sdat$x_train[,1:2], utility = DC)
 s3
 
-xgb2 <- xgb.load("save_test.dat")
-save(xgb, file = "test.dat")
 
-predict(xgb2, sdat$x_test)
+split_dat_gender <- function(dat, p1f, p2f) {
+  n <- nrow(dat)
+  male_index <- (dat[["sex_isFemale"]] == F)
+  nm <- sum(male_index)
+  n1m <- calc_n1m(n, nm, p1f, p2f)
+  X_m <- dat[male_index,-1]
+  y_m <- dat[male_index,1]
+  X_f <- dat[!male_index,-1]
+  y_f <- dat[!male_index,1]
+  
+  
+}
+
+
+# Calculates number of males n1m in the training set, where it is
+# assumed that we do not want to discard anybody.
+# n := number of people overall
+# nm := number of males overall
+# p1f := proportion of females to males in the training set
+# p2f := proportion of females to males in the test set
+calc_n1m <- function(n, nm, p1f, p2f) {
+  K1 <- p1f/(1-p1f)
+  K2 <- p2f/(1-p2f)
+  (n - nm*(K2+1))/(K1-K2)
+}
+
+n <- 14264
+nm <- 5765
+p1f <- 0.5
+p2f <- 0.9
+calc_n1m(n, nm, p1f, p2f)
+
+
+
+
+
 
 # UNUSED ------------------------------------------------------------------
 
@@ -114,3 +177,18 @@ predict(xgb2, sdat$x_test)
 #     xlab = "# Dropped Columns",
 #     ylab = "# Dropped Rows",
 #     lab = c(15,5,7))
+
+
+
+# # If we want to increase size of training set we need to drop females.
+# # Calculate number of discarded females df as a function of
+# # number of people in the training set, n1
+# # where the total number of people n and males nm are fixed:
+# n1_calc_df <- function(n1, p1f, p2f, n, nm) {
+#   K1 <- p1f/(1-p1f)
+#   K2 <- p2f/(1-p2f)
+#   df <- n - n1*(K1-K2)/(K1+1) - nm*(K2+1)
+#   return(df*(df > 0))
+# }
+# n1 <- 10000:14000
+# plot(n1, calc_df(n1, 0.5, 0.9, n, 5765), type = 'l')
