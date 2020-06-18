98404e7e768b72a3d12b36fd7101c784dbbd476b
Strumke <inga@strumke.com>
2020-06-18 12:10:30 +0200

    fixed dims issues but this might not be the right algorithm anyway

--- Julia/helpers.jl
+++ Julia/helpers.jl
@@ -2,8 +2,11 @@ using Statistics
 using Distributions
 
 function order_indices(x)
+
     sorted_indices = Dict() # dict of sorted index of each element
-    for (index, value) in enumerate(sort(x))
+    println(sort(x, dims = 1))
+
+    for (index, value) in enumerate(sort(x, dims = 1))
         sorted_indices[value] = index
     end
     
@@ -12,18 +15,35 @@ function order_indices(x)
 
 end
 
-function dcov(x, y)
-    # Huo Szekely Appendix A
-    #
-    Iˣ = order_indices(x) # indices if sorted
-    Iʸ = order_indices(y) # indices if sorted 
+function twodim(x)
+    if ndims(x) == 1
+        return reshape(x, (length(x), 1))
+    end
+    return x 
+end
 
-    # Sort x and y
+function dcov(x, y)
+
+    x = twodim(x)
+    y = twodim(y)
+
+    # Huo Szekely Appendix A
+    Iˣ = order_indices(x)
+    Iʸ = order_indices(y)
+
     sort!(x, dims=1)
     sort!(y, dims=1)
 
     s(l, i) = sum(l[1:i]) # Partial sum of l up until index i
-    alphaˣᵢ = Iˣ[i] - 1
+
+    αˣ(i) = Iˣ[i] - 1
+    αʸ(i) = Iʸ[i] - 1
+    
+    βˣ(i) = s(x, i) * (Iˣ[i] - 1)
+    βʸ(i) = s(y, i) * (Iʸ[i] - 1)
+
+    return αˣ(2), βʸ(2)
+
 
 end
 
@@ -33,8 +53,12 @@ end
 
 # TESTING 
 #x = rand(Uniform(0,1), 10, 3)
-#y = rand(Uniform(0,1), 10, 1)
+y = rand(Uniform(0,1), 10, 1)
 #println(cov(x, y))
 #println(cov(x))
 #println(cov(y))
 #println(var(x))
+x = [4, 8, 2]
+#println(order_indices(x))
+println(dcov(x,x))
+println(dcov(x,y))
