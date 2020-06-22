0e1d2e86a6081980fc91c58aa6f7d72ad0350f08
Strumke <inga@strumke.com>
2020-06-22 09:18:52 +0200

    why doesn't this work

--- Julia/helpers.jl
+++ Julia/helpers.jl
@@ -12,22 +12,23 @@ end
 
 function calc_mat(x)
     D = distance_matrix(x, x)
-    return (D .- mean(D, dims=1) .- mean(D, dims=2) .- mean(D))
+    return (D .- mean(D, dims=1) .- mean(D, dims=2) .+ mean(D))
 end
 
 function dcov(x, y)
 
     A = calc_mat(x)
     B = calc_mat(y)
-    return (norm((A .* B)))
+    return norm(A .* B)
 end
 
 
 # TESTING 
-x = rand(Uniform(0,1), 3, 3)
-y = rand(Uniform(0,1), 3, 3)
-println(x)
-println(y)
+#x = rand(Uniform(0,1), 3, 3)
+#y = rand(Uniform(0,1), 3, 3)
+x = [0.828353 0.43233 0.463169; 0.397422 0.273435 0.368917; 0.483459 0.851078 0.566512]
+y = [0.208873 0.815978 0.304595; 0.485556 0.256617 0.626528; 0.769359 0.552493 0.925741]
+#dcov = 0.2872514947935752
 
 println(dcov(x, y))
 
@@ -42,6 +43,8 @@ end
 # UNIVARIATE CASE: Huo Szekely Appendix A
 function order_indices(x)
 
+    # Python solution without dict:
+    # a = sorted(enumerate(sorted(enumerate(a), key=itemgetter(1))), key=itemgetter(1, 0))
     sorted_indices = Dict() # dict of sorted index of each element
     #for (index, value) in enumerate(sort(x, dims = 1))
     for (index, value) in enumerate(sort(x))
