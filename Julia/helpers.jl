50a7ab2f8591f0cb5f244749415aa800cd30b9df
Strumke <inga@strumke.com>
2020-06-22 10:39:25 +0200

    dcov and dcor in julia works

--- Julia/helpers.jl
+++ Julia/helpers.jl
@@ -16,21 +16,33 @@ function calc_mat(x)
 end
 
 function dcov(x, y)
-
     A = calc_mat(x)
     B = calc_mat(y)
-    return norm(A .* B)
+    #return norm(A .* B) # Wrong, don't know why
+    return sqrt(mean(A .* B))
 end
 
+function dcor(x, y)
+    return (dcov(x, y)) / (sqrt(dcov(x, x) * dcov(y, y)))
+end
 
-# TESTING 
+# === TESTING 
 #x = rand(Uniform(0,1), 3, 3)
 #y = rand(Uniform(0,1), 3, 3)
+
+# --- Values from python
 x = [0.828353 0.43233 0.463169; 0.397422 0.273435 0.368917; 0.483459 0.851078 0.566512]
 y = [0.208873 0.815978 0.304595; 0.485556 0.256617 0.626528; 0.769359 0.552493 0.925741]
 #dcov = 0.2872514947935752
+#dcor = 0.9610019939406783
+#x = [0.802453 0.903502 0.650715; 0.116538 0.781613 0.184347; 0.172204 0.0199412 0.687175]
+#y = [0.0180497 0.259578 0.464613; 0.211949 0.278084 0.96341; 0.701463 0.740318 0.319947]
+#dcov = 0.40846505175768183
+#dcor = 0.9849145696875032
+# ---
 
 println(dcov(x, y))
+println(dcor(x, y))
 
 function twodim(x)
     if ndims(x) == 1
