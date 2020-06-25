305916537d307d5fe3e033eed62833311d4186a0
Strumke <inga@strumke.com>
2020-06-25 10:39:32 +0200

    we have dcor and aidc functions in julia now. but damn are they slow

--- Julia/helpers.jl
+++ Julia/helpers.jl
@@ -31,6 +31,31 @@ function dcor(x, y)
     return 0
 end
 
+function aidc(x, y)
+    cov_y = cov(y)
+    cov_x = cov(x) # NB! This is cov(x.T) in python
+
+    #TODO:What if x is empty? then, cov(x)=-0.0 in Julia
+    if cov_x == -0.0
+        x_trans = x
+    elseif size(cov_x) == ()
+        inv_cov_x = 1.0/cov_x
+        x_trans = x * sqrt(inv(cov_x))
+    else
+        sqrt_inv_cov_x = sqrt(inv(cov_x))
+        if sqrt_inv_cov_x isa Array{Complex{Float64},2}
+            x_trans = x * convert(Array{Float64,2}, sqrt_inv_cov_x)
+        else
+            x_trans = x * sqrt(inv_cov_x)
+        end
+    end
+
+    inv_cov_y = 1 ./ cov_y
+    y_trans = y * sqrt(inv_cov_y)
+
+    return dcor(y_trans, x_trans)
+end
+
 function twodim(x)
     if ndims(x) == 1
         return reshape(x, (length(x), 1))
@@ -107,6 +132,8 @@ function test_dcov()
     dcov_true = 0.40846505175768183
     
     @assert round(dcov_true, digits=8) == round(dcov(x, y), digits=8)
+
+    println("DCOV function ok!")
 end
 
 function test_dcor()
@@ -122,7 +149,61 @@ function test_dcor()
     dcor_true = 0.9849145696875032
     
     @assert round(dcor_true, digits=8) == round(dcor(x, y), digits=8)
+
+    println("DCOR function ok!")
 end
+
+function test_aidc()
+
+    x =   [0.755635    0.345446  -0.688384
+          -1.11828    -1.68771   -0.596009
+          -2.38458    -0.121187  -0.815811
+           0.0603389   0.656752  -0.327731
+           0.826708    0.97178   -0.164207
+          -1.45166    -0.579462   1.4277  
+          -0.720464    0.60213   -0.244417
+           0.232181    0.96931    0.736506
+          -1.3447      1.12004   -1.21429 
+          -0.405403   -0.562174   0.581327]
+    y = [  0.23258329322511723
+           0.2833107386114472 
+          -1.2564352832581345 
+          -0.5874108883625386 
+          -0.9447399412637602 
+          -0.23829487490839948
+          -1.5497635035663024 
+           0.7354636701191419 
+           2.0315107752106654 
+           1.6625315369928169]
+    x1 = [ -0.341371   -0.133888  -1.26302   -0.467231    0.644707 
+          -0.0524715  -0.829657   0.600282   0.78509     1.49509  
+           0.081209   -1.60534   -0.467177  -0.715523   -1.31282  
+           0.597182    1.67758   -0.149649   0.198237    3.2101   
+          -0.782839    1.24472   -0.537776  -0.774976    0.611209 
+          -0.942106    1.4796     0.327017   1.66083    -0.0437138
+           0.674375   -1.82007    0.314038  -0.0269082   0.13076  
+           0.120167   -0.495699   2.79102   -0.0135147  -0.80674  
+          -1.3133     -0.678867  -0.125036   0.450812   -0.550585 
+          -0.358026    0.763886  -0.642018  -1.53592     0.910746 
+         ]
+    y1 = [  
+         0.31699426810558795 
+         1.34181444337193    
+        -0.508878033643789   
+         2.6854023615295426  
+        -0.7195432425919883  
+         0.4681875927260907  
+         0.027206421899317337
+         0.6396687066390925  
+         0.203168750153398   
+        -1.0873217125552856]
+    aidc_python = 0.5486719473485777
+    aidc_python1 = 0.7678647928797977
+    @assert round(aidc(x, y), digits=8) == round(aidc_python, digits=8)
+    @assert round(aidc(x1, y1), digits=8) == round(aidc_python1, digits=8)
+    println("AIDC function ok!")
+end
+test_aidc()
 
 # === TESTING 
 #dcov_univariate(x,x)
